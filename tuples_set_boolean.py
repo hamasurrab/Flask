eb4d5f6a23000c6da88923fecb9eb6312deed22b
hamasurrab2 <hamasurrab2@gmail.com>
2022-09-23 19:01:08 +0530

    tuples_set_boolean.

---
+++ tuples_set_boolean.py
@@ -0,0 +1,25 @@
+#tuples
+t=(1,2,3,'hamas')
+print(t)
+
+# sets
+x=set()
+print(x)
+
+x.add(1)
+x.add(2)
+print(x)
+
+
+
+my_list=[11,22,33,44,55,66]
+print(set(my_list))
+
+
+#boolean
+a=True
+b= False
+print (1<2)
+
+
+
