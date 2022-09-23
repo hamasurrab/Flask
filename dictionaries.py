9488455bd1662ca5d8a2ab50c3566ded880893ca
hamasurrab2 <hamasurrab2@gmail.com>
2022-09-23 18:21:13 +0530

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+d={"key1":"value1","key2":"value2"}
+print(d)
+
+
+
+
+
+
+
+# to get value by using key name-------
+salaries={"rohit":"100000","hamas":"222222"}
+print(salaries['hamas'])
--- list.py
+++ list.py
@@ -1,3 +1,15 @@
 my_list=[1,2,3,4,5,6,7,8,9]
+
+
+# to add in my_list---------it will add at last
 my_list.append('h')
+
+
+# to add in my_list--------at your index choice 
+
+my_list.insert(3,'hamas')
+
+# to remove from list -------we pop method
+my_list.pop(2)
+
 print(my_list)
