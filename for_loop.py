b15cdca652bcb8bea25378c6fd22637c44eeadd3
hamasurrab2 <hamasurrab2@gmail.com>
2022-09-27 16:15:26 +0530

    while loop and range

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,39 @@
+# -----for loop-----------
+
 my_iterable=[1,2,3]
 
 for item_name in my_iterable :
     print(item_name)
+print("------------------------------------------------------------------")
 
+
+myPairs=[('q',1),('b',2),('c',3)]
+
+for num,letter in myPairs:
+   
+    print (num)
+
+
+print("------------------------------------------------------------------")
+
+
+    # -----------while loop--------
+a=int(input("enter numa "))
+print (a)
+while a<2:
+    print(f"{a}")
+else:
+        print(5)
+
+print("------------------------------------------------------------------")
+# -------------range---------
+for x in range(0,6):
+    print(x)
+print("------------------------------------------------------------------")
+
+    #------if you want to jump num like 2,4,5,6......--
+for y in range (0,11,2):
+        print (y)
+print("---------------------to check given elemnt is presnt in it---------------------------------------------")
+
+print('s' in "asdfghjklkjhgfdsaasdfghjklkjhgfds")
